8b9cdd144ef89bdb3c590cfa7c07dd44863e530b
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-08-09 13:10:47 +0000

    [Update]トップページ、アバウトページへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :public do
-    get '/'
-    get '/about'
+    get '/' => 'homes#top'
+    get '/about' => 'homes#about'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
