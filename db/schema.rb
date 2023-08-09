044a73366b8e3c748153b596cac908d6230043a7
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-08-09 09:13:51 +0000

    [Add]Cart_itemモデルと各カラム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_09_084049) do
+ActiveRecord::Schema.define(version: 2023_08_09_090738) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "customer_id", null: false
+    t.integer "amount", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
