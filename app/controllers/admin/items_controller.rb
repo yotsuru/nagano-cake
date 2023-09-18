54c54352103378a385d16a2c0bd947f527f3ce50
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-09-18 01:16:20 +0000

    商品編集画面　実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,6 +18,19 @@ class Admin::ItemsController < ApplicationController
       @item = Item.find(params[:id])
     end
     
+    def edit
+      @item = Item.find(params[:id])
+    end
+    
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+      else
+      render "show"
+      end
+    end
+    
     private
  
     def item_params
