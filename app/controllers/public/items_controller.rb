e09ee8dae61e2a31737cfc679a9f53531fe8f5c9
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-09-23 05:08:35 +0000

    public商品一覧画面　実装

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,6 @@
 class Public::ItemsController < ApplicationController
+    
+    def index
+        @items = Item.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ namespace :public do
     get '/customers/mypage' => 'customers#show'
     get '/customers/information/edit' => 'customers#edit'
     patch '/customers' => 'customers#update', as: 'update_customer'
+    
+    resources :items,only: [:index,:show]
   end
   
   
