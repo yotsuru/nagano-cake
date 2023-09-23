d35cefbb41638abe52e13412affd7b5aff3620f3
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-09-23 05:20:48 +0000

    public商品詳細画面　実装

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,4 +3,9 @@ class Public::ItemsController < ApplicationController
     def index
         @items = Item.all
     end
+    
+    def show
+        @items = Item.all
+        @item = Item.find(params[:id])
+    end
 end
