4e3297f06fadd8e8c2a7daf57bc3059c76cf1e7a
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-09-25 11:32:34 +0000

    Itemモデル、Cart_itemモデルに記述

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,11 @@
 class CartItem < ApplicationRecord
+    belongs_to :item
+	belongs_to :customer
+
+	validates :customer_id, :item_id, :amount, presence: true
+	validates :amount, numericality: { only_integer: true }
+
+    def subtotal
+     item.with_tax_price * amount
+    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
     has_many :order_details, dependent: :destroy
     has_many :cart_items, dependent: :destroy
+    
+    def with_tax_price
+     (price * 1.1).floor
+    end
 end
