fa86a09187364741b32f1bc71c27280f3cf623a0
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-09-25 13:45:06 +0000

    カート表示画面　実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,6 +3,7 @@ class Public::CartItemsController < ApplicationController
     before_action :authenticate_customer!
 
     def index
+     @cart_items = current_customer.cart_items
      @cart_items = current_cart
     end
     
