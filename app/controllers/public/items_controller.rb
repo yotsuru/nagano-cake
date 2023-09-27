be1616a60a80c6766d34d3c502e700e0666aed72
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-09-27 11:47:25 +0000

    ページネーション追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,9 @@
 class Public::ItemsController < ApplicationController
     
+    before_action :authenticate_customer!, only: [:show]
+    
     def index
-        @items = Item.all
+        @items = Item.page(params[:page]).per(10)
     end
     
     def show
@@ -9,4 +11,9 @@ class Public::ItemsController < ApplicationController
         @item = Item.find(params[:id])
         @cart_item = CartItem.new
     end
+    
+    private
+	def item_params
+		parmas.require(:item).permit(:image ,:name, :introduction, :price)
+	end
 end
