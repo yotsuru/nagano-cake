f8d47087cba26677be8bd011d2188f61d272591d
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-09-27 10:06:06 +0000

    [Update]カート機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,20 +3,25 @@ class Public::CartItemsController < ApplicationController
     before_action :authenticate_customer!
 
     def index
-     #@cart_items = current_customer.cart_items
-     @cart_items = current_customer
+     @cart_items = current_customer.cart_items.all
     end
     
     def update
+     @cart_item = Cart_item.find(params[:id])
      @cart_item.update(amount: params[:cart_item][:amount].to_i)
-     @cart_items = current_cart
     end
 
     def create
-     @cart_item = current_cart.new(cart_item_params)
-     @post_image.user_id = current_user.id
-     @post_image.save
-     redirect_to post_images_path
+     @cart_item = current_customer.cart_items.new(cart_item_params)
+     if CartItem.find_by(item_id: @cart_item.item_id)
+       cart_item = CartItem.find_by(item_id: @cart_item.item_id)
+       @amount = cart_item.amount+ @cart_item.amount
+       cart_item.update_attribute(:amount, @amount)
+       @cart_item.delete
+     end
+       @cart_item.save
+       redirect_to public_cart_items_path
+     
     end
 
     def destroy
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -7,5 +7,6 @@ class Public::ItemsController < ApplicationController
     def show
         @items = Item.all
         @item = Item.find(params[:id])
+        @cart_item = CartItem.new
     end
 end
