4c84a49748c2e843285b41eaadbc8d43c7a8277e
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-10-19 06:14:42 +0000

    [Update]注文情報確認ページ

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,22 +11,24 @@ class Public::OrdersController < ApplicationController
     end
     
     def confirm
+      @cart_items = current_customer.cart_items.all
+      #@total_price = current_customer.cart_items.price(@cart_items)
       @order = Order.new(order_params)
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
-      @order.name = current_customer.first_name + current_customer.last_name
+      @order.receiver = current_customer.first_name + current_customer.last_name
     end
     
     def create
       @order = current_customer.orders.new(order_params)
       @order.save
-      flash[:notice] = "ご注文が確定しました。"
       redirect_to public_orders_complete_path
       # カートの商品を確定済みテーブルに移動させる
       current_customer.cart_items.each do |cart_item|
       # ロジック
+      end
     end
-   
+    
     def complete
     end
 
@@ -42,5 +44,4 @@ def order_params
   params.require(:order).permit(:payment_method, :postal_code, :address, :name)
  
 end
-
 end
